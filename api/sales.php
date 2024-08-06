386c7a2080fba0084baffe6002dc1d0a62dc6c79
Mel Macario <xmelmacario@gmail.com>
2024-08-06 17:37:13 +0800

    added getZReportWithSelectedDate function sales.php

--- api/sales.php
+++ api/sales.php
@@ -90,6 +90,54 @@ class Sales
     }
   }
 
+  function getZReportWithSelectedDate($json)
+  {
+    // {"date":"2024-08-06"}
+    include "connection.php";
+    $json = json_decode($json, true);
+    try {
+      $sql = "SELECT a.sale_id, d.user_fullname, a.sale_cashTendered, a.sale_change, a.sale_totalAmount, a.sale_date, 
+      b.sale_item_productId, b.sale_item_quantity, b.sale_item_price, c.prod_name AS product_name FROM tbl_sales a 
+      INNER JOIN tbl_sale_item b ON a.sale_id = b.sale_item_saleId 
+      INNER JOIN tbl_products c ON b.sale_item_productId = c.prod_id 
+      INNER JOIN tbl_users d ON a.sale_userId = d.user_id 
+      WHERE DATE(a.sale_date) = :date 
+      ORDER BY a.sale_date DESC";
+      $stmt = $conn->prepare($sql);
+      $stmt->bindParam(":date", $json["date"]);
+      $stmt->execute();
+
+      $sales = [];
+      if ($stmt->rowCount() > 0) {
+        $rs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($rs as $row) {
+          $saleId = $row['sale_id'];
+          if (!isset($sales[$saleId])) {
+            $sales[$saleId] = [
+              'user_username' => $row['user_fullname'],
+              'sale_cashTendered' => $row['sale_cashTendered'],
+              'sale_change' => $row['sale_change'],
+              'sale_totalAmount' => $row['sale_totalAmount'],
+              'sale_date' => $row['sale_date'],
+              'items' => []
+            ];
+          }
+          $sales[$saleId]['items'][] = [
+            'sale_item_productId' => $row['sale_item_productId'],
+            'sale_item_quantity' => $row['sale_item_quantity'],
+            'sale_item_price' => $row['sale_item_price'],
+            'product_name' => $row['product_name']
+          ];
+        }
+      }
+
+      return json_encode(array_values($sales));
+    } catch (PDOException $e) {
+      return 0;
+    }
+  }
+
   function getShiftReport($json)
   {
     // {"userId":1}
@@ -142,7 +190,7 @@ class Sales
 function getCurrentDate()
 {
   $today = new DateTime("now", new DateTimeZone('Asia/Manila'));
-  return $today->format('Y-m-d');
+  return $today->format('Y-m-d H:i:s');
 }
 
 $json = isset($_POST["json"]) ? $_POST["json"] : "0";
@@ -159,6 +207,9 @@ switch ($operation) {
     break;
   case "getShiftReport":
     echo $sales->getShiftReport($json);
+    break;
+  case "getZReportWithSelectedDate":
+    echo $sales->getZReportWithSelectedDate($json);
     break;
   default:
     echo "Wala kay gi butang nga operation sa ubos HAHAHAHA bobo";
